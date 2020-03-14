a7a6d3a3d0525061a86d4ea672a405596f3ba85a
diode <dualvectorfoil@163.com>
2020-03-15 04:17:35 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/DualVectorFoil/solar/router"
+)
+
+func main() {
+	router.Init()
+}
---
+++ router/router.go
@@ -0,0 +1,23 @@
+package router
+
+import (
+	"github.com/DualVectorFoil/solar/util/jsonUtil"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Init() {
+
+
+	router := gin.Default()
+	router.NoRoute()
+
+	dataRouter := router.Group("/data_service")
+	dataRouter.POST("/test", nil)
+
+	router.Run(":13145")
+}
+
+func notFound(c *gin.Context) {
+	c.String(http.StatusNotFound, jsonUtil.JsonResp(http.StatusNotFound, nil, "404 router not found"))
+}
